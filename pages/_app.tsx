612e978c8ff1158e2d4f1eba66835f042c48934b
KelvinTrinh <thkien85@Gmail.com>
2023-12-25 14:10:24 +0700

    Add New
    Add New

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,17 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
+import { Sepolia } from "@thirdweb-dev/chains";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "goerli";
+// const activeChain = "goerli";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider 
-      activeChain={activeChain}
+      activeChain={Sepolia}
       clientId={process.env.CLIENT_ID}  
     >
       <ChakraProvider>
